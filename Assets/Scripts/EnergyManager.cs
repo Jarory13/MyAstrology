6924baf1570343bed86515547790b7aa282b4526
Jarory de Jesus <jdejesus@fiea.local>
2017-12-19 16:28:18 -0500

    Add tappable stuff and work on energy system

--- Assets/Scripts/EnergyManager.cs
+++ Assets/Scripts/EnergyManager.cs
@@ -9,6 +9,14 @@ public class EnergyManager : MonoBehaviour {
     public static EnergyManager instance;
     public static int EnergyMeter;
 
+    [SerializeField]
+    private int EnergyLimit = 20;
+    [SerializeField]
+    private float timeBetweenRefills = 3 * 60;
+    private float timeToNextRefill;
+    private int EnergyToAdd = 1;
+
+
     private void Awake()
     {
         if (instance == null)
@@ -25,11 +33,54 @@ public class EnergyManager : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-		
-	}
+        //TODO: Get date time since last start in player pref then set that as last time in player prefs
+
+        //int lastTime = PlayerPrefs.GetInt("LastTime");
+        //if (lastTime == 0)
+        //{
+        //    Debug.Log("Last time doesnt exist ");
+        //    lastTime = DateTime.
+        //    PlayerPrefs.SetInt("LastTime", lastTime);
+        //}
+        //else
+        //{
+        //    Debug.Log("Last time is " + lastTime);
+        //}
+
+        timeToNextRefill = Time.time + timeBetweenRefills;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        TrackEnergyTime();
+
+    }
+
+    public void ReduceEnergy(int amount)
+    {
+        EnergyMeter -= amount;
+        if (EnergyMeter < 0)
+        {
+            EnergyMeter = 0;
+        }
+    }
+
+    public void AddEnergy(int amount)
+    {
+        EnergyMeter += amount;
+        if (EnergyMeter > EnergyLimit)
+        {
+            EnergyMeter = EnergyLimit;
+        }
+    }
+
+    private void TrackEnergyTime()
+    {
+        if (Time.time > timeToNextRefill)
+        {
+            AddEnergy(EnergyToAdd);
+            timeToNextRefill = Time.time + timeBetweenRefills;
+        }
+    }
 }
