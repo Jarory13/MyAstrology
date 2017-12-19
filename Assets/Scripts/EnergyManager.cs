6901918b1f87cc456f48d7939637025e99148cb0
Jarory de Jesus <jdejesus@fiea.local>
2017-12-19 15:13:02 -0500

    Initial commit

---
+++ Assets/Scripts/EnergyManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+using UnityEngine;
+
+public class EnergyManager : MonoBehaviour {
+
+    public static EnergyManager instance;
+    public static int EnergyMeter;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+
+        PlayerData data = new PlayerData();
+        data.Energy = EnergyManager.EnergyMeter;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+}
+
+[Serializable]
+class PlayerData
+{
+    public int Energy { get; set; }
+
+}
